3c4f1aa85d86cfeeff71432bc9ae29a161423589
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 02:49:52 +0300

    Scripted auto-commit on change (2019-11-23 02:49:52) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -1,5 +1,6 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
@@ -8,11 +9,46 @@ public class TestData implements Serializable {
     private TestPackage parentPackage;
     private String testName;
     private String actualResult;
-    private String rightresult;
+    private String rightResult;
     private Object[] parameters;
 
-    public TestEntity(@JsonProperty("testName")
+    @JsonCreator
+    public TestEntity(@JsonProperty("testName")String testName,
+                      @JsonProperty("rightResult")String rightResult,
+                      @JsonProperty("parameters")Object[] parameters){
+        this.testName = testName;
+        this.rightResult = rightResult;
+        this.parameters = parameters;
+    }
 
-    )
+    public void setActualResult(String actualResult){
+        this.actualResult = actualResult;
+    }
+
+    public void setParentPackage(TestPackage parentPackage){
+        this.parentPackage = parentPackage;
+    }
+
+    public String getActualResult(){
+        return this.actualResult;
+    }
+
+    public TestPackage getParentPackage(){
+        return this.parentPackage;
+    }
+
+    public  String getTestName(){
+        return this.testName;
+    }
+
+    public  String getRightResult(){
+        return this.rightResult;
+    }
+
+    public Object[] getParameters(){
+        return this.parameters;
+    }
+
+
 
 }

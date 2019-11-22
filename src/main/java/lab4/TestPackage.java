de838f1e7a60e919ca0e63f83aaf8a6e875ec8ce
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 02:57:10 +0300

    Scripted auto-commit on change (2019-11-23 02:57:10) by gitwatch.sh

--- src/main/java/lab4/TestPackage.java
+++ src/main/java/lab4/TestPackage.java
@@ -1,12 +1,40 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class TestPackage implements Serializable {
     private String packageId;
     private String jsFunctionName;
-      
+    private String jsCode;
+    private List<TestData> tests;
 
+    public TestPackage(@JsonProperty("packageId") String packageId,
+                       @JsonProperty("jsCode") String jsCode,
+                       @JsonProperty("jsFunctionName") String jsFunctionName,
+                       @JsonProperty("tests") List<TestData> tests){
+        this.packageId = packageId;
+        this.jsFunctionName = jsFunctionName;
+        this.jsCode = jsCode;
+        this.tests = tests;
+    }
 
+    public String getPackageId(){
+        return this.packageId;
+    }
+
+    public String getJsFunctionName(){
+        return this.jsFunctionName;
+    }
+
+    public String getJsCode(){
+        return this.jsCode;
+    }
+
+    public List<TestData> getTests(){
+        return this.tests;
+    }
 
 }

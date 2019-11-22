1ef67a191753a2e785d0d71c2d2363ef0aa83616
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 02:38:46 +0300

    Scripted auto-commit on change (2019-11-23 02:38:46) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -3,6 +3,8 @@ package lab4;
 import java.io.Serializable;
 
 public class TestData implements Serializable {
-    private Test
+    private TestPackage;
+    private String testName;
+    private String
 
 }

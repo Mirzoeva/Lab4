1de3b7a6d4655ba352d51db992b8b876adf06202
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 02:50:21 +0300

    Scripted auto-commit on change (2019-11-23 02:50:21) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -14,7 +14,7 @@ public class TestData implements Serializable {
 
     @JsonCreator
     public TestEntity(@JsonProperty("testName")String testName,
-                      @JsonProperty("rightResult")String rightResult,
+                      @JsonProperty("expectedResult")String rightResult,
                       @JsonProperty("parameters")Object[] parameters){
         this.testName = testName;
         this.rightResult = rightResult;

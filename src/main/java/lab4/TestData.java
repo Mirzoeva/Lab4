3d537ddda68e6ffe545fbf6a55558d4017f0198b
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 02:42:26 +0300

    Scripted auto-commit on change (2019-11-23 02:42:26) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -1,12 +1,18 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public class TestData implements Serializable {
-    private TestPackage;
+    private TestPackage parentPackage;
     private String testName;
     private String actualResult;
     private String rightresult;
     private Object[] parameters;
+
+    public TestEntity(@JsonProperty("testName")
+
+    )
 
 }

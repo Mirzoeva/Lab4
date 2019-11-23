01575fdbdfa4e0f9081c4b5149f8e9521a99cfa5
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 03:22:57 +0300

    Scripted auto-commit on change (2019-11-23 03:22:57) by gitwatch.sh

--- src/main/java/lab4/TestResult.java
+++ src/main/java/lab4/TestResult.java
@@ -1,11 +1,23 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public class TestResult implements Serializable {
     private String actualResult;
     private String rightResult;
     private Boolean succes;
+
+    @JsonCreator
+    public TestResult(@JsonProperty("right") String rightResult,
+                      @JsonProperty("actual")String actualResult,
+                      @JsonProperty("success") Boolean succes){
+        this.rightResult = rightResult;
+        this.actualResult = actualResult;
+        this.succes = succes;
+    }
 
 
 

27bcb81639f20434b49d9f8ac6a125dc8de5b3c1
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 03:40:39 +0300

    Scripted auto-commit on change (2019-11-23 03:40:39) by gitwatch.sh

--- src/main/java/lab4/TesterActor.java
+++ src/main/java/lab4/TesterActor.java
@@ -1,17 +1,44 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class TesterActor extends AbstractActor {
+    private ActorRef storageActor;
 
-    TesterActor(){
+    TesterActor(ActorRef storageActor){
+        this.storageActor = storageActor;
+    }
 
+    private String runTest(TestData testData) throws ScriptException, NoSuchMethodException{
+        ScriptEngine engine = new
+                ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(testData.getParentPackage().getJsCode());
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(testData.getParentPackage().getJsFunctionName(), testData.getParameters().toString());
+    }
+
+    private TestData checkResult(TestData testData){
+        try {
+            String getResult = runTest(testData);
+            testData.setActualResult(getResult);
+        } catch (Exception exception){
+            String error = exception.toString();
+            testData.setActualResult(error);
+        }
+        return testData;
     }
 
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create()
-//                .match()
+                .match(TestData.class, msg -> storageActor.tell(checkResult(msg), ActorRef.noSender()))
+                .build();
     }
 }

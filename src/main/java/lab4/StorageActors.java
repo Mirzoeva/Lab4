a3c8e6b79186026214b207e20bc4e23305c3859e
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 03:13:17 +0300

    Scripted auto-commit on change (2019-11-23 03:13:17) by gitwatch.sh

--- src/main/java/lab4/StorageActors.java
+++ src/main/java/lab4/StorageActors.java
@@ -4,15 +4,40 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageActors extends AbstractActor {
 
     private Map<String, ArrayList<TestData>> store = new HashMap<>();
 
+    private void putTest(TestData testData){
+        String packageId = testData.getParentPackage().getPackageId();
+        if (this.store.containsKey(packageId)){
+            this.store.get(packageId).add(testData);
+        } else {
+            ArrayList<TestData> tests = new ArrayList<>();
+            tests.add(testData);
+            this.store.put(packageId, tests);
+        }
+    }
+
+    private ArrayList<TestData> getTests(String packageId) throws Exception{
+        if (this.store.containsKey(packageId)){
+            return this.store.get(packageId);
+        } else {
+            throw  new Exception("No such package");
+        }
+    }
+
+//    private
+
+
 
     @Override
     public Receive createreceive(){
         return ReceiveBuilder.create()
-//                .match()
+                .match(TestData.class, test -> this.putTest(test))
+//                .match(String.class, id -> sender().tell())
     }
 }

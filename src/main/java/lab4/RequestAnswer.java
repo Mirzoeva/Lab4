8e5bcb9f786f764077f6faf379d17cc1d302cae1
Mirzoeva <vselennaya314@gmail.com>
2019-11-23 03:26:17 +0300

    Scripted auto-commit on change (2019-11-23 03:26:17) by gitwatch.sh

--- src/main/java/lab4/RequestAnswer.java
+++ src/main/java/lab4/RequestAnswer.java
@@ -13,7 +13,23 @@ public class RequestAnswer implements Serializable {
     @JsonCreator
     public RequestAnswer(@JsonProperty("packageId") String packageId,
                          @JsonProperty("tests") List<TestResult> tests){
-        
-
+        this.packageId = packageId;
+        this.tests = tests;
     }
+
+    public String getPackageId(){
+        return  this.packageId;
+    }
+
+    public List<TestResult> getTests(){
+        return this.tests;
+    }
+
+    public void setPackageId(String packageId){
+        this.packageId = packageId;
+    }
+
+    public void setTests(List)
+
+
 }
